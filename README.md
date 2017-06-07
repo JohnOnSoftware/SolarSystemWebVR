cc991b5e2c07d13dd5dad8ed757c44f18f7bfc14
Zhong Wu <wuzhong@domiler.com>
2017-06-07 21:48:13 +0800

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+Solar WebVR
+
+Based onReact VR 1.0.0
+
+You can see the tutorial of how this app was built at https://www.pluralsight.com/guides/front-end-javascript/getting-started-with-react-vr.
+
+
+Zhong Wu
+MIT
---
+++ button.js
@@ -0,0 +1,34 @@
+// 'use strict'
+
+import React from 'react'
+import { View, Text, VrButton, StyleSheet } from 'react-vr'
+
+export default class Button extends React.Component {
+  constructor () {
+    super()
+    this.styles = StyleSheet.create({
+        button:{
+            margin:0.05,
+            height:0.4,
+            backgroundColor: 'red',
+        },
+        text:{
+            fontSize:0.4,
+            textAlign: 'center'
+        }
+
+    })
+  }
+
+  render () {
+    return (
+      <VrButton style={this.styles.button}
+        onClick={() => this.props.callback()} >
+        <Text style={this.styles.text}>
+            {this.props.text}
+        </Text>
+      </VrButton>
+    )
+  }
+
+}
